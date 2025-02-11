a684fe25c1e13428221c3895364864c8c0b8bd8f
alihm961 <alihasanmazloum@gmail.com>
2025-02-11 17:29:12 +0100

    Create <> First python commit

---
+++ conditions.py
@@ -0,0 +1,24 @@
+# age logic
+#age = int(input("please enter your age: "))
+
+#check = age > 12
+
+#if check:
+ #   print("You are eligible to run this code")
+#else:
+#    print("Not eligible")
+    
+# Grades logic 
+
+grade = int(input("Please enter your grade: "))
+
+if grade > 90:
+    print("A")
+elif grade > 80:
+    print("B")
+elif grade > 70:
+    print("C")
+elif grade > 70:
+    print("D")
+else:
+    Print("F")
