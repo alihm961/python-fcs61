27b21ce833fa424923aca3dd99b811d82e029da6
alihm961 <alihasanmazloum@gmail.com>
2025-02-12 19:17:28 +0100

    Exercise 2 Even or Odd checker

--- assignments.py
+++ assignments.py
@@ -1,13 +1,23 @@
 # Exercise 1 // Movie ticket price calculator
 
-age = int(input("Enter your age: "))
+#age = int(input("Enter your age: "))
 
-if age < 5:
-    price = 0
-elif age <= 12:
-    price = 5
-elif age <=18:
-    price = 8
+#if age < 5:
+ #   price = 0
+#elif age <= 12:
+ #   price = 5
+#elif age <=18:
+ #   price = 8
+#else:
+ #   price = 10
+#print(f"The ticket price is {price} $")
+
+
+# Exercise 2 // Even or Odd checker
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is even.")
 else:
-    price = 10
-print(f"The ticket price is {price} $")
+    print("The number is odd.")
